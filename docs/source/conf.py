b4e7f7539efdb799a2708110c82cd8d25f3bbe8e
GorArzanyanAUA <155971957+GorArzanyanAUA@users.noreply.github.com>
2025-11-20 23:43:35 +0400

    Update project details in Sphinx configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'RLDroneSim'
+copyright = '2025, TA'
+author = 'TA'
 
 release = '0.1'
 version = '0.1.0'
@@ -13,7 +13,6 @@ version = '0.1.0'
 
 extensions = [
     'sphinx.ext.duration',
-    'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
